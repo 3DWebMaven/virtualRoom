dbab7beb0253240d53b84c8923e92bb656317cfb
3dwebmaven <armando.3dweb@gmail.com>
2025-03-25 02:17:32 +0900

    add intro video

--- src/Room.js
+++ src/Room.js
@@ -1,12 +1,17 @@
-import { useGLTF } from "@react-three/drei"
+import { useVideoTexture, useTexture, useGLTF } from "@react-three/drei"
 import { RigidBody } from "@react-three/rapier"
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF("/showroom2.glb")
+  const room = useGLTF("/showroom2.glb")
+  const videoTexture = useVideoTexture('../intro.webm')
   return (
     <group {...props} dispose={null}>
       <RigidBody type="fixed" colliders="trimesh">
-        <primitive object={useGLTF("/showroom2.glb").scene} />
+        <primitive object={room.scene} />
+        <mesh position={[-5,10,47]} rotation={[0, Math.PI, 0]}>
+          <meshBasicMaterial map={videoTexture} side={2} color={'#fff'} />
+          <planeGeometry args={[30,15]}/>
+        </mesh>
       </RigidBody>
     </group>
   )
