9a53ece988c8cc81ba039c75345ac098100ec1a2
3dwebmaven <armando.3dweb@gmail.com>
2025-03-25 10:49:45 +0900

    remove soft shadow

--- src/App.js
+++ src/App.js
@@ -9,20 +9,6 @@ import { Model as Room } from "./Room"
 import { Player } from "./Player"
 import { Html } from "@react-three/drei"
 
-// function Light() {
-//   const ref = useRef()
-//   useFrame((state, delta) => {
-//     easing.dampE(ref.current.rotation, [(state.pointer.y * Math.PI) / 50, (state.pointer.x * Math.PI) / 20, 0], 0.2, delta)
-//   })
-//   return (
-//     <group ref={ref}>
-//       <directionalLight position={[5, 5, -8]} castShadow intensity={5} shadow-mapSize={2048} shadow-bias={-0.001}>
-//         <orthographicCamera attach="shadow-camera" args={[-8.5, 8.5, 8.5, -8.5, 0.1, 20]} />
-//       </directionalLight>
-//     </group>
-//   )
-// }
-
 const InidiatorComponent = (props) => {
   const near = 8;
   const [hover, set] = useState(null)
