b5eb0a7841edc5dba079f5c26b7b93a063ac6341
3dwebmaven <armando.3dweb@gmail.com>
2025-04-16 10:59:50 +0900

    temp for removing

--- src/Room.js
+++ src/Room.js
@@ -9,13 +9,13 @@ export function Model(props) {
   const room = useGLTF("/showroom_o.glb")
   const videoTexture = useVideoTexture('../intro.webm')
   const npcModels = [
-    { rotation: [0, 0.1, 0], position: [-108, -5, 30], scale: 10, url: "/models/woman_5_o.glb" },
-    { rotation: [0, 1.5, 0], position: [-130, -5, 0], scale: 4.5, url: "/models/woman_4_o.glb" },
-    { rotation: [0, 3, 0], position: [-87, -5, -30], scale: 9, url: "/models/man_3_o.glb" },
-    { rotation: [0, -0.5, 0], position: [-30, -5, 25], scale: 0.1, url: "/models/man_4_o.glb" },
-    { rotation: [0, 2.5, 0], position: [-110, -5, -25], scale: 3.8, url: "/models/man_2_o.glb" },
-    { rotation: [0, 0, 0], position: [-50, -5, -18], scale: 9.5, url: "/models/employee.glb" },
-    { rotation: [0, 2, 0], position: [-90, -5, 25], scale: 10, url: "/models/woman_6_o.glb"},
+    // { rotation: [0, 0.1, 0], position: [-108, -5, 30], scale: 10, url: "/models/woman_5_o.glb" },
+    // { rotation: [0, 1.5, 0], position: [-130, -5, 0], scale: 4.5, url: "/models/woman_4_o.glb" },
+    // { rotation: [0, 3, 0], position: [-87, -5, -30], scale: 9, url: "/models/man_3_o.glb" },
+    // { rotation: [0, -0.5, 0], position: [-30, -5, 25], scale: 0.1, url: "/models/man_4_o.glb" },
+    // { rotation: [0, 2.5, 0], position: [-110, -5, -25], scale: 3.8, url: "/models/man_2_o.glb" },
+    // { rotation: [0, 0, 0], position: [-50, -5, -18], scale: 9.5, url: "/models/employee.glb" },
+    // { rotation: [0, 2, 0], position: [-90, -5, 25], scale: 10, url: "/models/woman_6_o.glb"},
     // { rotation: [0, 1.7, 0], position: [-50, -5, -2], scale: 10, url: "/models/man_5.glb"},
     { rotation: [0, 0, 0], position: [-50, -5, -2], scale: 10, url: "/models/E.glb"} ,
     { rotation: [0, 0, 0], position: [-40, -5, -5], scale: 10, url: "/models/E_1.glb"} ,
